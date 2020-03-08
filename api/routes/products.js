ebe45bf4cb9edacde1ed6f811fe623762101b22d
Xinrui Ma <fullstackguy@gmail.com>
2020-03-08 00:08:11 -0800

    create patch

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,9 +5,20 @@ const mongoose = require('mongoose');
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling GET Request to /products'
+  Product.find().exec()
+  .then(docs => {
+    console.log(docs);
+    res.status(200).json(docs);
   })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: err
+    })
+  })
+  // res.status(200).json({
+  //   message: 'Handling GET Request to /products'
+  // })
 })
 
 router.post('/', (req, res, next) => {
@@ -64,14 +75,41 @@ router.get('/:productId', (req, res, next) => {
 });
 
 router.patch('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Update product!'
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.update({ _id: id }, { $set: updateOps })
+  .exec()
+  .then(result => {
+    console.log(result);
+    res.status(200).json(result)
   })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: err
+    })
+  });
+
+  // res.status(200).json({
+  //   message: 'Update product!'
+  // })
 });
 
 router.delete('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Delete product!'
+  const id = req.params.productId;
+  Product.remove({ _id: id })
+  .exec()
+  .then(result => {
+    res.status(200).json(result)
+  })
+  .catch(err => {
+    console.log(err);
+    err.status(500).json({
+      error: err
+    })
   })
 })
 
